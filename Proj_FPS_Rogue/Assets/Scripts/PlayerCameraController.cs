f9e33c52033774b1be4214ee3992934cc7649dc3
FTWRahul <r.poundarik@gmail.com>
2020-02-07 17:28:47 -0500

    Player camera controller

---
+++ Proj_FPS_Rogue/Assets/Scripts/PlayerCameraController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour
+{
+    public static float mouseSensitivity = 2f;
+    public Transform playerBody;
+    [SerializeField] private Vector2 yMinMax;
+    float xAxisClamp = 0;
+
+    void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        this.enabled = true;
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        RotateCamera();
+    }
+
+    void RotateCamera()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X");
+        float mouseY = Input.GetAxisRaw("Mouse Y");
+
+        float rotAmountX = mouseX * mouseSensitivity;
+        float rotAmountY = mouseY * mouseSensitivity;
+
+        xAxisClamp -= rotAmountY;
+
+        Vector3 targetRotCam = transform.rotation.eulerAngles;
+        Vector3 targetRotBody = playerBody.rotation.eulerAngles;
+
+        targetRotCam.x -= rotAmountY;
+        targetRotCam.z = 0;
+        targetRotBody.y += rotAmountX;
+
+        if(xAxisClamp > 90)
+        {
+            xAxisClamp = 90;
+            targetRotCam.x = 90;
+        }
+        else if(xAxisClamp < -90)
+        {
+            xAxisClamp = -90;
+            targetRotCam.x = 270;
+        }
+        transform.rotation = Quaternion.Euler(targetRotCam);
+        playerBody.rotation = Quaternion.Euler(targetRotBody);
+    }
+
+    public static void ChangeSensitivity(float newSen)
+    {
+        mouseSensitivity = newSen;
+    }
+}
