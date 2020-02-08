25e7e81e629d8d18b04caa149e5c5c4e95120182
Jane <glazychevaeo@gmail.com>
2020-02-08 15:16:52 -0500

    Dummy enemy health

---
+++ Proj_FPS_Rogue/Assets/Scripts/HealthState.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+public class HealthState : MonoBehaviour, IReceiveDamage
+{
+    public float health;
+    public float maxHealth;
+
+    private void Start()
+    {
+        SetMaxHealth();
+    }
+
+    private void Update()
+    {
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void SetMaxHealth()
+    {
+        health = maxHealth;
+    }
+    
+    public void ApplyDamage(float damage)
+    {
+        if (health <= 0)
+            return;
+
+        health -= damage;
+        if (health <= 0)
+        {
+            health = 0;
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
+
+public interface IReceiveDamage
+{
+    void ApplyDamage(float damage);
+}
