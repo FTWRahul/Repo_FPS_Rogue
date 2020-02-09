b302dbc673fa54615d4d6701c0af1ee0ebc09cda
Jane <glazychevaeo@gmail.com>
2020-02-09 17:38:15 -0500

    Enemy nav mesh

---
+++ Proj_FPS_Rogue/Assets/Scripts/Enemy/EnemyMovementController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Enemy
+{
+    public class EnemyMovementController : MonoBehaviour
+    {
+        #region Movement settings
+
+        public float targetStoppingDistance;
+        public float movementSpeed;
+        
+        #endregion
+
+        #region References
+
+        private Transform _target;
+        private Vector3 TargetPosition => _target.position;
+        private Vector3 _desirePosition;
+        
+        private NavMeshAgent _navMeshAgent;
+
+        #endregion
+
+        private void Start()
+        {
+            _target = FindObjectOfType<CharacterData>().transform;
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+            _navMeshAgent.speed = movementSpeed;
+            _navMeshAgent.stoppingDistance = targetStoppingDistance;
+        }
+
+        private void Update()
+        {
+            if (Vector3.Distance(transform.position, TargetPosition) > targetStoppingDistance)
+            {
+                CalculateDesirePosition();
+
+                _navMeshAgent.SetDestination(_desirePosition);
+            }
+        }
+
+        void CalculateDesirePosition()
+        {
+            if(NavMesh.SamplePosition(TargetPosition, out var myNavHit, targetStoppingDistance, -1))
+            {
+                _desirePosition = myNavHit.position;
+            }
+        }
+    }
+}
